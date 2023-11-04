8aa8ea0d48443516d589f14902b634ec4e7b5164
Japon Phan <phanjapan100@gmail.com>
2023-11-04 22:47:44 +0700

    second commit in branch mvvm

--- app/src/main/java/kh/edu/rupp/ite/visitmekotlin/ui/viewmodel/ProvincesViewModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/visitmekotlin/ui/viewmodel/ProvincesViewModel.kt
@@ -1,10 +1,18 @@
 package kh.edu.rupp.ite.visitmekotlin.ui.viewmodel
 
+import kh.edu.rupp.ite.visitmekotlin.service.ApiService
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProvincesViewModel {
 
     fun loadProvinceList() {
-        val retrofit = Retrofit.Builder()
+        val httpClient = Retrofit.Builder()
+            .baseUrl("https://tests3bk.s3.ap-southeast-1.amazonaws.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = httpClient.create(ApiService::class.java)
+
     }
 }
